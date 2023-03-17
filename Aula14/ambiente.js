e929de54f160bd0978506edd5333e0880b367905
Bubinoso <luizcamolez@gmail.com>
2023-03-17 11:00:58 -0300

    while, dowhile and for

---
+++ Aula14/ambiente.js
@@ -0,0 +1,27 @@
+/*
+c = 1
+do
+{
+    console.log(c)
+    c++
+} while (c<= 5)
+
+while (c<= 5)
+{
+    console.log(c)
+    c++
+}
+
+for (c=1;c<=5;c++) {
+    console.log(c)
+}
+
+*/
+
+console.log('Começando...')
+for(c=1;c<=5;c++)
+{
+    console.log(c)
+}
+console.log('Acabou.')
+
