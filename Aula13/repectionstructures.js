22a0787fd9b49a84aecb93146f4533d1e142c3de
Bubinoso <luizcamolez@gmail.com>
2023-03-17 10:59:50 -0300

    while and dowhile

---
+++ Aula13/repectionstructures.js
@@ -0,0 +1,16 @@
+
+/* 
+var c = 1
+while (c <= 100)
+{
+    console.log(`${c}`) // estrutura de repetição com teste lógico no ínicio
+    c++
+}
+*/
+var c = 1
+do
+{
+    console.log(`${c}`) // estrutura de repetição com teste lógico no final
+    c++
+}
+while (c <= 6)
